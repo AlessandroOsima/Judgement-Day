b360bbc9c96a3517fd287bb91af3a9e0bdad2cb6
daff0111 <daff0111.df@gmail.com>
2013-11-12 21:11:13 +0100

    I've put the Pathfinding with NavMesh and the Unit Animation as a Prefab. work with it

---
+++ prototypes/Pathfinder_Animation/Assets/Scripts/Unit_Pathfinding.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit_Pathfinding : MonoBehaviour {
+	float dt;
+	float speed = 5f;
+	NavMeshAgent controller;
+	public Vector3 target;
+	//int t_target = 1;
+	// Use this for initialization
+	void Start () {
+		controller=GetComponent<NavMeshAgent>();
+		target = GameObject.Find("_target1").transform.position;
+		controller.destination = target;
+		//animator = GameObject.Find("Person").GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (t_target==1){
+			target = GameObject.Find("_target1").transform.position;
+			controller.destination = target;
+		}
+		if (t_target==2){
+			target = GameObject.Find("_target2").transform.position;
+			controller.destination = target;
+		}*/
+		
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.name=="_target1"){
+			target = GameObject.Find("_target2").transform.position;
+			controller.destination = target;
+			//t_target=2;
+		}
+		if (other.name=="_target2"){
+			target = GameObject.Find("_target1").transform.position;
+			controller.destination = target;
+			//t_target=1;
+		}
+		//animator.Play("PoseLib");
+	}
+}
