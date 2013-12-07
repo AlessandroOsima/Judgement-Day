52a9cdd2b6bc4f528deb421064a7d11adc23d021
taglia90 <taglia_1@hotmail.it>
2013-12-07 16:13:20 +0100

    Added
    Added globalFear to global manager. Added SountrackManager

--- main/JDMaster/Assets/Scripts/GlobalManager.cs
+++ main/JDMaster/Assets/Scripts/GlobalManager.cs
@@ -1,223 +1,249 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 public enum EndGameState
 {
-	VictoryEasy,
-	VictoryMedium,
-	VictoryHard,
-	VictoryCustom,
-	Defeat
+    VictoryEasy,
+    VictoryMedium,
+    VictoryHard,
+    VictoryCustom,
+    Defeat
 }
 
 /*Global Manager stores all the global game variables, it's possible to be notified when a variable changes via the on*Changed events
  *All the variable are positive, they are set to 0 if a negative value is passed.
  */
 
-public class GlobalManager : MonoBehaviour 
+public class GlobalManager : MonoBehaviour
 {
-	//Public variables
-	public static string npcsTag = "Person";
-	//Victory conditions
-	public int easyVictory;
-	public int mediumVictory;
-	public int hardVictory;
-	public bool standardVictoryConditions = true;
-	//Private variables
-	static GlobalManager _globalManager;
-	public int initialSouls;
-	int _souls;
-	int _score;
-	int _population;
-	int _globalFear = 0;
-	bool firstUpdate = false;
-	//Events
-	public delegate void OnVarChanged(int pastVar, int newVar);
-	public delegate void OnEndGame(EndGameState endGameState);
-	public event OnEndGame onEndGame;
-	public event OnVarChanged onSoulsChanged;
-	public event OnVarChanged onPopulationChanged;
-	public event OnVarChanged onScoreChanged;
+    //Public variables
+    public static string npcsTag = "Person";
+    //Victory conditions
+    public int easyVictory;
+    public int mediumVictory;
+    public int hardVictory;
+    public bool standardVictoryConditions = true;
+    //Private variables
+    static GlobalManager _globalManager;
+    public int initialSouls;
+    int _souls;
+    int _score;
+    int _population;
+    float _globalFear;
+    bool firstUpdate = false;
+    //Events
+    public delegate void OnVarChanged(int pastVar, int newVar);
+    public delegate void OnEndGame(EndGameState endGameState);
+    public event OnEndGame onEndGame;
+    public event OnVarChanged onSoulsChanged;
+    public event OnVarChanged onPopulationChanged;
+    public event OnVarChanged onScoreChanged;
 
-	//Properties
-	public static GlobalManager globalManager
-	{
-		get
-		{
-			return _globalManager;
-		}
-	}
-	
-	public int souls 
-	{
-		get
-		{
-			return _souls;
-		}
-		set
-		{
-			if(value < 0)
-			{
-				if(onSoulsChanged != null)
-					onSoulsChanged(_souls,0);
+    void Start()
+    {
+        score = 0;
+        souls = initialSouls;
+        population = GameObject.FindGameObjectsWithTag(npcsTag).Length;
 
-				_souls = 0;
-			}
-			else
-			{
-				if(onSoulsChanged != null)
-					onSoulsChanged(_souls,value);
+    }
 
-				_souls = value;
-			}
-		}
-	}
+    void Awake()
+    {
+        if (globalManager == null) //this way you cannot create more than one Player_Score instance
+            _globalManager = this;
+    }
 
-	public int score
-	{
-		get
-		{
-			return _score;
-		}
-		set
-		{
-			if(value < 0)
-			{
-				if(onScoreChanged != null)
-					onScoreChanged(_score, 0);
+    void Update()
+    {
+        if (!firstUpdate) //necessary to correctly set the GUI elements for the first time (forces On*Changed callbacks to be sent and the GUI to be updated)
+        {
+            score = score;
+            souls = souls;
+            population = population;
+            firstUpdate = true;
+        }
+    }
 
-				_score = 0;
-			}
-			else
-			{
-				if(onScoreChanged != null)
-					onScoreChanged(_score, value);
+    #region Properties
+    public static GlobalManager globalManager
+    {
+        get
+        {
+            return _globalManager;
+        }
+    }
 
-				_score = value;
-			}
-		}
-	}
-	public int population
-	{
-		get
-		{
-			return _population;
-		}
+    public int souls
+    {
+        get
+        {
+            return _souls;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                if (onSoulsChanged != null)
+                    onSoulsChanged(_souls, 0);
 
-		set
-		{
-			if(value <= 0)
-			{
-				if(onPopulationChanged != null)
-					onPopulationChanged(_population,0);
+                _souls = 0;
+            }
+            else
+            {
+                if (onSoulsChanged != null)
+                    onSoulsChanged(_souls, value);
 
-				_population = 0;
+                _souls = value;
+            }
+        }
+    }
 
-				if(score < easyVictory && standardVictoryConditions)
-				{
-					if(onEndGame != null)
-						onEndGame(EndGameState.Defeat);
-				}
-				else if(score >= easyVictory &&  standardVictoryConditions)
-				{
-					if(onEndGame != null)
-						onEndGame(EndGameState.VictoryEasy);
-				}
-				else if(score >= mediumVictory && standardVictoryConditions)
-				{
-					if(onEndGame != null)
-						onEndGame(EndGameState.VictoryMedium);
-				}
-				else if(score >= hardVictory && standardVictoryConditions)
-				{
-					if(onEndGame != null)
-						onEndGame(EndGameState.VictoryHard);
-				}
+    public int score
+    {
+        get
+        {
+            return _score;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                if (onScoreChanged != null)
+                    onScoreChanged(_score, 0);
 
-			}
-			else
-			{
-				if(onPopulationChanged != null)
-					onPopulationChanged(_population,value);
+                _score = 0;
+            }
+            else
+            {
+                if (onScoreChanged != null)
+                    onScoreChanged(_score, value);
 
-				_population = value;
-			}
-		}
-	}
-	public int globalFear
-	{
-		get
-		{
-			return _globalFear;
-		}
-		set
-		{
-			if(value < 0)
-				_globalFear = 0;
-			else
-				_globalFear = value;
-		}
-	}
+                _score = value;
+            }
+        }
+    }
 
-	void Awake()
-	{
-		if(globalManager == null) //this way you cannot create more than one Player_Score instance
-			_globalManager = this;
-	}
+    public int population
+    {
+        get
+        {
+            return _population;
+        }
 
-	void Start() 
-	{
-		score = 0;
-		souls = initialSouls;
-		population = GameObject.FindGameObjectsWithTag(npcsTag).Length;
+        set
+        {
+            if (value <= 0)
+            {
+                if (onPopulationChanged != null)
+                    onPopulationChanged(_population, 0);
 
-	}
-	
-	void Update () 
-	{
-		if(!firstUpdate) //necessary to correctly set the GUI elements for the first time (forces On*Changed callbacks to be sent and the GUI to be updated)
-		{
-			score = score;
-			souls = souls;
-			population = population;
-			firstUpdate = true;
-		}
-	}
-	
-	public void decrementSouls(int x)
-	{
-		souls = souls - x;
-	}
+                _population = 0;
 
-	public void incrementSouls(int x)
-	{
-		souls = souls + x;
-	}
-	
-	public void decrementPopulation(int x)
-	{
-		population = population - x;
-	}
-	public void incrementPopulation(int x)
-	{
-		population = population + x;
-	}
+                if (score < easyVictory && standardVictoryConditions)
+                {
+                    if (onEndGame != null)
+                        onEndGame(EndGameState.Defeat);
+                }
+                else if (score >= easyVictory && standardVictoryConditions)
+                {
+                    if (onEndGame != null)
+                        onEndGame(EndGameState.VictoryEasy);
+                }
+                else if (score >= mediumVictory && standardVictoryConditions)
+                {
+                    if (onEndGame != null)
+                        onEndGame(EndGameState.VictoryMedium);
+                }
+                else if (score >= hardVictory && standardVictoryConditions)
+                {
+                    if (onEndGame != null)
+                        onEndGame(EndGameState.VictoryHard);
+                }
 
-	public void incrementScore(int x)
-	{
-		score = score + x;
-	}
+            }
+            else
+            {
+                if (onPopulationChanged != null)
+                    onPopulationChanged(_population, value);
 
-	public void decrementScore(int x)
-	{
-		score = score - x;
-	}
+                _population = value;
+            }
+        }
+    }
 
-	public void levelIsCompleted(EndGameState state)
-	{
-		if(onEndGame != null)
-			onEndGame(state);
-	}
+    public float globalFear
+    {
+        get
+        {
+            return FindAverageFear();
+        }
+        set
+        {
+            if (value < 0)
+                _globalFear = 0;
+            else
+                _globalFear = FindAverageFear();
+        }
+    }
+    #endregion
+
+    // Find the people's average fear
+    public float FindAverageFear()
+    {
+        // Find all game objects with tag Person
+        GameObject[] npcsList;
+        npcsList = GameObject.FindGameObjectsWithTag(npcsTag);
+        int totalFear = 0;
+        int numberOfPeople;
+        // Iterate through them and find the total fear and number of people
+        for (numberOfPeople = 0; numberOfPeople < npcsList.Length; numberOfPeople++)
+        {
+           // Debug.Log("Paure singole: " + npcsList[numberOfPeople].GetComponent<PersonStatus>().Fear);
+            totalFear += npcsList[numberOfPeople].GetComponent<PersonStatus>().Fear;
+        }
+        float averageFear = totalFear / numberOfPeople;
+        return averageFear;
+    }
+
+
+    #region Increment-Decrement
+    public void decrementSouls(int x)
+    {
+        souls = souls - x;
+    }
+
+    public void incrementSouls(int x)
+    {
+        souls = souls + x;
+    }
+
+    public void decrementPopulation(int x)
+    {
+        population = population - x;
+    }
+
+    public void incrementPopulation(int x)
+    {
+        population = population + x;
+    }
+
+    public void incrementScore(int x)
+    {
+        score = score + x;
+    }
+
+    public void decrementScore(int x)
+    {
+        score = score - x;
+    }
+    #endregion
+
+    public void levelIsCompleted(EndGameState state)
+    {
+        if (onEndGame != null)
+            onEndGame(state);
+    }
 
 }
---
+++ main/JDMaster/Assets/Scripts/GlobalSoundsManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalSoundsManager : MonoBehaviour
+{
+
+    public AudioClip defaultClip;
+    public AudioClip happyClip;
+    public AudioClip happyClip2;
+    public AudioClip scaredClip;
+    public AudioClip scaredClip2;
+    public AudioClip warriedClip;
+    //public AudioClip myPlaylist[];
+
+    private GameObject globalManager;
+    private float fear;
+
+    void Start()
+    {
+        globalManager = GameObject.Find("Global");
+
+        // AudioSource.PlayClipAtPoint(defaultClip, Camera.main.transform.position);
+
+        if (audio.isPlaying)                    // if there is audio playing
+            audio.Stop();                       // stop it
+
+        audio.clip = happyClip;               // set the audio clip 
+        audio.Play();
+
+        audio.loop = true;
+        audio.playOnAwake = true;
+    }
+
+    void Update()
+    {
+
+        fear = globalManager.GetComponent<GlobalManager>().globalFear;
+
+        if (fear >= 0f && fear <= 30f && audio.clip != happyClip)
+        {
+            // Debug.Log("Entro in HAPPY CLIP");
+            if (audio.isPlaying)                    // if there is audio playing
+                audio.Stop();                       // stop it
+
+            audio.clip = happyClip;                 // set the audio clip 
+            audio.Play();
+            audio.loop = true;
+        }
+
+        if (fear > 30f && fear <= 60f && audio.clip != warriedClip)
+        {
+            // Debug.Log("Entro in WARRIED CLIP");
+            if (audio.isPlaying)                    // if there is audio playing
+                audio.Stop();                       // stop it
+
+            audio.clip = warriedClip;                 // set the audio clip 
+            audio.Play();
+            audio.loop = true;
+        }
+
+        if (fear > 60f && audio.clip != scaredClip)
+        {
+            // Debug.Log("Entro in SCARED CLIP");
+            if (audio.isPlaying)                    // if there is audio playing
+                audio.Stop();                       // stop it
+
+            audio.clip = scaredClip;                 // set the audio clip 
+            audio.Play();
+            audio.loop = true;
+        }
+    }
+}
