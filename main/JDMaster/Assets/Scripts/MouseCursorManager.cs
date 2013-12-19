aec8eb80dfb819f763df22c1918633c2dca915c5
taglia90 <taglia_1@hotmail.it>
2013-12-19 16:01:01 +0100

    Added MouseCursorManager

---
+++ main/JDMaster/Assets/Scripts/MouseCursorManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursorManager : MonoBehaviour
+{
+    public Texture2D originalCursor;
+    public int cursorSizeX; // set to width of your cursor texture
+    public int cursorSizeY; // set to height of your cursor texture
+
+    private bool _showCursor = true;
+    private static MouseCursorManager _mouseManager;
+
+    public MouseCursorManager()
+    {
+
+    }
+
+    void Start()
+    {
+        //Screen.showCursor = false;      //hide standard cursor
+        //Screen.lockCursor = true;
+    }
+
+    void Awake()
+    {
+        if (mouseManager == null) //this way you cannot create more than one Player_Score instance
+            _mouseManager = this;
+    }
+
+
+    void OnGUI()
+    {
+
+        if (_showCursor == true)
+        {
+            GUI.DrawTexture(new Rect(Input.mousePosition.x - cursorSizeX / 2 + 1, (Screen.height - Input.mousePosition.y) - cursorSizeY / 2 + 1, cursorSizeX, cursorSizeY), originalCursor);
+        }
+
+    }
+
+    #region Properties
+    public bool ShowCursor
+    {
+        get
+        {
+            return _showCursor;
+        }
+        set
+        {
+            _showCursor = value;
+        }
+    }
+
+    public static MouseCursorManager mouseManager
+    {
+        get
+        {
+            return _mouseManager;
+        }
+    }
+    #endregion
+}
--- main/JDMaster/Assets/Scripts/PowersScripts/BasePowerDealer.cs
+++ main/JDMaster/Assets/Scripts/PowersScripts/BasePowerDealer.cs
@@ -3,130 +3,131 @@ using System.Collections;
 using System;
 using System.Reflection;
 
-using UnityEngine;
-using System.Collections;
-
-public abstract class PowerEffect 
+public abstract class PowerEffect
 {
-	protected BasePowerDealer owner;
-	public void initialize(BasePowerDealer owner)
-	{
-		this.owner = owner;
-	}
-	public abstract bool OnTriggerEnterOverride(Collider other, Power owner);
-	public abstract void deliverPowerEffects(PersonStatus status, AnimationScript animator, UnitNavigationController navigator);
-	public abstract void runNavigatorUpdate(UnitNavigationController navigator);
-	public abstract void deliverOnCollisionEffect(Collider other, PersonStatus status);
+    protected BasePowerDealer owner;
+    public void initialize(BasePowerDealer owner)
+    {
+        this.owner = owner;
+    }
+    public abstract bool OnTriggerEnterOverride(Collider other, Power owner);
+    public abstract void deliverPowerEffects(PersonStatus status, AnimationScript animator, UnitNavigationController navigator);
+    public abstract void runNavigatorUpdate(UnitNavigationController navigator);
+    public abstract void deliverOnCollisionEffect(Collider other, PersonStatus status);
 }
 
-public class BasePowerDealer : Power 
+public class BasePowerDealer : Power
 {
-	//Public vars
-	public float speed = 25f;
-	public int Fear = 70;
-	public int Highness = 0;
-	public GameObject godRay;
-	public GameObject particleEffect;
-	public PowerEffect powerEffect;
-	public float distance = 40;
-	//Private vars
-	bool ready = true;
-	int souls;
-	Vector3 startPosition;
-	Vector3 Location; //for the preview of the power
-	float previousParticlePosY = 0;
-	
-	// Use this for initialization
-	void Start () {
-		startPosition = transform.position; //saves the start position
-		//pscore = GameObject.Find("_Global").GetComponent<Player_Score>(); //initializing the script
-		//stat = GameObject.Find("Human").GetComponent<Status>();
-	}
+    //Public vars
+    public float speed = 25f;
+    public int Fear = 70;
+    public int Highness = 0;
+    public GameObject godRay;
+    public GameObject particleEffect;
+    public PowerEffect powerEffect;
+    public float distance = 40;
+    //Private vars
+    private bool ready = true;
+    private int souls;
+    private Vector3 startPosition;
+    private Vector3 Location; //for the preview of the power
+    private float previousParticlePosY = 0;
+    private Ray ray;
 
-	void OnEnable() //The Power is activated by the power manager, initialize stuff here, do NOT use any find* function (Slooow !!)
-	{
-		godRay.SetActive(true);
-		particleEffect.SetActive(true);
-		ready = true;
-	}
-	
-	void OnDisable() //The Power is deactivated by the power manager, deinitialize stuff here, do NOT use any find* function
-	{
-		godRay.SetActive(false);
-		particleEffect.SetActive(false);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = transform.position; //saves the start position
+        //pscore = GameObject.Find("_Global").GetComponent<Player_Score>(); //initializing the script
+        //stat = GameObject.Find("Human").GetComponent<Status>();
+    }
 
-	public override void deliverPowerEffects(PersonStatus status, AnimationScript animator, UnitNavigationController navigator)
-	{
-	}
+    void OnEnable() //The Power is activated by the power manager, initialize stuff here, do NOT use any find* function (Slooow !!)
+    {
+        MouseCursorManager.mouseManager.ShowCursor = false;
+        godRay.SetActive(true);
+        particleEffect.SetActive(true);
+        ready = true;
+    }
 
-	public override void runNavigatorUpdate(UnitNavigationController navigator)
-	{
-	}
+    void OnDisable() //The Power is deactivated by the power manager, deinitialize stuff here, do NOT use any find* function
+    {
+        godRay.SetActive(false);
+        particleEffect.SetActive(false);
+        MouseCursorManager.mouseManager.ShowCursor = true;
+    }
 
+    public override void deliverPowerEffects(PersonStatus status, AnimationScript animator, UnitNavigationController navigator)
+    {
+    }
 
-	//COLLISION
-	public override void OnTriggerEnter(Collider other)
-	{
-		string effectName = this.gameObject.name + "Effect";
-
-		//Type type = Type.GetType(effectName);
-		PowerEffect effect = (PowerEffect)Assembly.GetExecutingAssembly().CreateInstance(effectName);
-		effect.initialize(this);
+    public override void runNavigatorUpdate(UnitNavigationController navigator)
+    {
+    }
 
 
+    //COLLISION
+    public override void OnTriggerEnter(Collider other)
+    {
+        string effectName = this.gameObject.name + "Effect";
 
-		if(!effect.OnTriggerEnterOverride(other,this))
-		{
+        //Type type = Type.GetType(effectName);
+        PowerEffect effect = (PowerEffect)Assembly.GetExecutingAssembly().CreateInstance(effectName);
+        effect.initialize(this);
 
-			if(other.tag == GlobalManager.npcsTag)
-			{
-				PersonStatus person = other.GetComponent<PersonStatus>();
 
-				if(person.UnitStatus != PersonStatus.Status.Dead && person.IsAValidTarget)
-				{
-					audio.Play();
-					person.ActivePower =  effect;
-				}
-			}
-		}
-	}
 
-	// Update is called once per frame
-	void Update () 
-	{
-		souls = GlobalManager.globalManager.souls;
+        if (!effect.OnTriggerEnterOverride(other, this))
+        {
 
-		if(ready)
-		{
-			//PREVIEW
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			Location = ray.origin + (ray.direction * distance);
-			Location.y = 0;
-			transform.position = Location;
-			particleEffect.transform.position = new Vector3(particleEffect.transform.position.x,17.5f,particleEffect.transform.position.z);
+            if (other.tag == GlobalManager.npcsTag)
+            {
+                PersonStatus person = other.GetComponent<PersonStatus>();
 
-			if(Input.GetMouseButton(0))
-			{ 
-				Debug.Log("HIT for :" + this.name);
-				ready = false;
-				previousParticlePosY = particleEffect.transform.position.y;
-			}
-		}
-			
+                if (person.UnitStatus != PersonStatus.Status.Dead && person.IsAValidTarget)
+                {
+                    audio.Play();
+                    person.ActivePower = effect;
+                }
+            }
+        }
+    }
 
-		if(!ready)
-		{
 
-			particleEffect.transform.position = particleEffect.transform.position + Vector3.down*speed*Time.deltaTime;
+    void OnGUI()
+    {
+        //souls = GlobalManager.globalManager.souls;
 
-			if(particleEffect.transform.position.y <= 0)
-			{
-				ready = true;
-				particleEffect.transform.position = new Vector3(Location.x,previousParticlePosY,Location.z);
-				GlobalManager.globalManager.decrementSouls(price);
-			}
-		}
-	
-	 }
+        if (ready)
+        {
+            //PREVIEW
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Location.x = ray.origin.x + (ray.direction.x * 40);
+            Location.y = 0;
+            Location.z = ray.origin.z + (ray.direction.z * 40);
+            transform.position = Location;
+            particleEffect.transform.position = new Vector3(particleEffect.transform.position.x, 17.5f, particleEffect.transform.position.z);
+
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("HIT for :" + this.name);
+                ready = false;
+                previousParticlePosY = particleEffect.transform.position.y;
+            }
+        }
+        else
+        {
+
+            particleEffect.transform.position = particleEffect.transform.position + Vector3.down * speed * Time.deltaTime;
+
+            if (particleEffect.transform.position.y <= 0)
+            {
+                ready = true;
+                particleEffect.transform.position = new Vector3(Location.x, previousParticlePosY, Location.z);
+                GlobalManager.globalManager.decrementSouls(price);
+            }
+        }
+
+    }
 }
