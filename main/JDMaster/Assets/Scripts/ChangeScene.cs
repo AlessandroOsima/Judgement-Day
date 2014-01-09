7211cc0ea1d4d7c407b66b91932b7d073e95ae96
taglia90 <taglia_1@hotmail.it>
2014-01-09 16:07:03 +0100

    added level selection scene

---
+++ main/JDMaster/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public string sceneName;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            try
+            {
+                Application.LoadLevel(sceneName);
+            }
+            catch
+            {
+                Debug.LogError("Scene does NOT exist!");
+            }
+    }
+}
