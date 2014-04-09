3da382a5b67ec8e661c6dfcf0e42a8492d324e48
taglia90 <taglia_1@hotmail.it>
2014-04-09 19:22:28 +0200

    Fixed movements with arrows and zoom in main menu

--- main/JDMaster/Assets/Camera/CameraControlMenu.cs
+++ main/JDMaster/Assets/Camera/CameraControlMenu.cs
@@ -6,64 +6,80 @@ public class CameraControlMenu : MonoBehaviour
     public float speed = 90;
     public float horizontalMovement = 100;
     public float rayLenght = 1.5f;
+    public float tweenDuration = 1f;
     public List<Vector3> positions;
+
 
     float distFromMin;
     float distFromMax;
 
     static TweenPosition tweenPos;
-    private int isolaSelezionata = 0;
-    int numeroIsole = 0;
+    static int selectedIsland = 0;
+    int numberOfIsles = 0;
+    bool running = false;
+    public static List<Vector3> staticPositions;
 
-    Transform target;
-    float smooth = 5;
     Vector3 direction;
     Vector3 origin;
     Vector3 movement;
     Vector3 destination;
 
+    int moveDirection = 0;
+
     void Start()
     {
         tweenPos = camera.GetComponent<TweenPosition>();
         tweenPos.ignoreTimeScale = true;
-        tweenPos.duration = 2f;
+        tweenPos.duration = tweenDuration;
+        tweenPos.style = UITweener.Style.Once;
         tweenPos.method = UITweener.Method.EaseInOut;
-        numeroIsole = positions.Count;
-
+        numberOfIsles = positions.Count;
+        staticPositions = positions;
     }
 
     void Update()
     {
-        System.Threading.Thread.Sleep(200);
-        // KeyEventArgs.SuppressKeyPress = false;
+        if (running) return;
 
         direction = new Vector3(0, 0, 0);
         origin = transform.position;
+
+        float movement = Input.GetAxis("Horizontal");
+        Debug.Log(movement);
+
+        if (movement == 0) return;
+        else if (movement > 0)
+        {
+            ++selectedIsland;
+
+            if (selectedIsland > numberOfIsles - 1)
+            {
+                selectedIsland--;
+                return;
+            }
+        }
+        else if (movement < 0)
+        {
+            --selectedIsland;
+
+            if (selectedIsland < 0) selectedIsland = 0;
+        }
+        running = true;
         tweenPos.from = origin;
+        tweenPos.to = positions[selectedIsland];
+        tweenPos.ResetToBeginning();
+        tweenPos.onFinished.Clear();
+        EventDelegate.Add(tweenPos.onFinished, OnTweenFinished);
+        tweenPos.PlayForward();
 
-        direction.x = Input.GetAxis("Horizontal") * horizontalMovement;
-
-        movement = transform.TransformDirection(direction);
-
-        destination = origin + movement;
-
-
-        if (destination.x > origin.x && isolaSelezionata < numeroIsole - 1)     //metto il  -1 perchè comincio a contare da 0
-        {
-            isolaSelezionata++;
-            tweenPos.to = positions[isolaSelezionata];
-            tweenPos.PlayForward();
-        }
-        else if (destination.x < origin.x && isolaSelezionata > 0)
-        {
-            isolaSelezionata--;
-            tweenPos.to = positions[isolaSelezionata];
-            tweenPos.PlayForward();
-
-        }
-        // KeyEventArgs.SuppressKeyPress = true;
     }
 
+    void OnTweenFinished()
+    {
+        running = false;
+    }
+
+    #region Properties
     public static TweenPosition cameraTweenPosition
     {
         get
@@ -72,4 +88,24 @@ public class CameraControlMenu : MonoBehaviour
         }
     }
 
+    public static List<Vector3> IslandsPositions
+    {
+        get
+        {
+            return staticPositions;
+        }
+    }
+
+    public static int SelectedIsle
+    {
+        get
+        {
+            return selectedIsland;
+        }
+    }
+    
+    
+    #endregion
+
 }
+
--- main/JDMaster/Assets/Scripts/LevelsScripts/ChangeScene.cs
+++ main/JDMaster/Assets/Scripts/LevelsScripts/ChangeScene.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System;
 
@@ -8,11 +8,17 @@ public class ChangeScene : MonoBehaviour
 
 
     public string sceneName;
+    public float zoomDuration = 4f;
     public Vector3 vectorZoom;
 
     string path = @"sceneName.dat";
-
     static TweenPosition tweenPos;
+    bool running = true;
+
+
+
+    private List<Vector3> islandsPositions;
+    private int selectedIsland = 0;
 
     void OnMouseDown()
     {
@@ -22,41 +28,56 @@ public class ChangeScene : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
 
+
+            islandsPositions = CameraControlMenu.IslandsPositions;
+            selectedIsland = CameraControlMenu.SelectedIsle;
             tweenPos = CameraControlMenu.cameraTweenPosition;   //riprendo l'elemento TweenPosition della camera
-            tweenPos.duration = 40f;
+            tweenPos.duration = zoomDuration;
             tweenPos.ignoreTimeScale = true;
-            tweenPos.method = UITweener.Method.EaseInOut;
-            tweenPos.from = transform.position;
+            tweenPos.method = UITweener.Method.EaseIn;
+            tweenPos.from = islandsPositions[selectedIsland];
             tweenPos.to = vectorZoom;
+            tweenPos.ResetToBeginning();
+            tweenPos.onFinished.Clear();
+            EventDelegate.Add(tweenPos.onFinished, OnTweenFinished);
             tweenPos.PlayForward();
 
-            System.Threading.Thread.Sleep(200);
-
-
-            try
-            {
-
-                //Pass the filepath and filename to the StreamWriter Constructor
-                StreamWriter sw = new StreamWriter(path, false);
-
-                //Write a line of text
-                sw.WriteLine(sceneName);
-                sw.Flush();
-
-                //Close the file
-                sw.Close();
-            }
-            catch (Exception e)
-            {
-                Debug.Log("Exception in WriteFile" + e.Message);
-            }
-
-            if (Application.GetStreamProgressForLevel("LoadingScreen") == 1)
-            {
-                Application.LoadLevel("LoadingScreen");
-            }
-
         }
+    }
+
+    void Update()
+    {
+        if (running) return;
+
+
+        try
+        {
+
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter(path, false);
+
+            //Write a line of text
+            sw.WriteLine(sceneName);
+            sw.Flush();
+
+            //Close the file
+            sw.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Exception in WriteFile" + e.Message);
+        }
+
+        if (Application.GetStreamProgressForLevel("LoadingScreen") == 1)
+        {
+            Application.LoadLevel("LoadingScreen");
+        }
+    }
+
+
+    private void OnTweenFinished()
+    {
+        running = false;
     }
 
 }
