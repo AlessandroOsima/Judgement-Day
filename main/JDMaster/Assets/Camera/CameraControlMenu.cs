f1107e2708d6e24d5222e181da86b73d01e49ac6
taglia90 <taglia_1@hotmail.it>
2014-04-09 11:43:41 +0200

    Added TweenPosition for scrolling and zooming in the Menu

---
+++ main/JDMaster/Assets/Camera/CameraControlMenu.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlMenu : MonoBehaviour
+{
+    public float speed = 90;
+    public float horizontalMovement = 100;
+    public float rayLenght = 1.5f;
+    public List<Vector3> positions;
+
+    float distFromMin;
+    float distFromMax;
+
+    static TweenPosition tweenPos;
+    private int isolaSelezionata = 0;
+    int numeroIsole = 0;
+
+    Transform target;
+    float smooth = 5;
+    Vector3 direction;
+    Vector3 origin;
+    Vector3 movement;
+    Vector3 destination;
+
+    void Start()
+    {
+        tweenPos = camera.GetComponent<TweenPosition>();
+        tweenPos.ignoreTimeScale = true;
+        tweenPos.duration = 2f;
+        tweenPos.method = UITweener.Method.EaseInOut;
+        numeroIsole = positions.Count;
+
+    }
+
+    void Update()
+    {
+        System.Threading.Thread.Sleep(200);
+        // KeyEventArgs.SuppressKeyPress = false;
+
+        direction = new Vector3(0, 0, 0);
+        origin = transform.position;
+        tweenPos.from = origin;
+
+        direction.x = Input.GetAxis("Horizontal") * horizontalMovement;
+
+        movement = transform.TransformDirection(direction);
+
+        destination = origin + movement;
+
+
+        if (destination.x > origin.x && isolaSelezionata < numeroIsole - 1)     //metto il  -1 perchè comincio a contare da 0
+        {
+            isolaSelezionata++;
+            tweenPos.to = positions[isolaSelezionata];
+            tweenPos.PlayForward();
+        }
+        else if (destination.x < origin.x && isolaSelezionata > 0)
+        {
+            isolaSelezionata--;
+            tweenPos.to = positions[isolaSelezionata];
+            tweenPos.PlayForward();
+
+        }
+        // KeyEventArgs.SuppressKeyPress = true;
+    }
+
+    public static TweenPosition cameraTweenPosition
+    {
+        get
+        {
+            return tweenPos;
+        }
+    }
+
+}
--- main/JDMaster/Assets/Scripts/LevelsScripts/ChangeScene.cs
+++ main/JDMaster/Assets/Scripts/LevelsScripts/ChangeScene.cs
@@ -6,23 +6,38 @@ using System;
 public class ChangeScene : MonoBehaviour
 {
 
+
     public string sceneName;
+    public Vector3 vectorZoom;
+
     string path = @"sceneName.dat";
-    
+
+    static TweenPosition tweenPos;
+
     void OnMouseDown()
     {
+
+
+
         if (Input.GetMouseButton(0))
         {
 
+            tweenPos = CameraControlMenu.cameraTweenPosition;   //riprendo l'elemento TweenPosition della camera
+            tweenPos.duration = 40f;
+            tweenPos.ignoreTimeScale = true;
+            tweenPos.method = UITweener.Method.EaseInOut;
+            tweenPos.from = transform.position;
+            tweenPos.to = vectorZoom;
+            tweenPos.PlayForward();
 
+            System.Threading.Thread.Sleep(200);
 
-          
 
             try
             {
 
                 //Pass the filepath and filename to the StreamWriter Constructor
-                StreamWriter sw = new StreamWriter(path,false);
+                StreamWriter sw = new StreamWriter(path, false);
 
                 //Write a line of text
                 sw.WriteLine(sceneName);
