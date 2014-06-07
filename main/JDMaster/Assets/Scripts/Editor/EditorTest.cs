089ab69414e387230bd15d238eed0a0c9a8247b6
Alessandro Osima <alex_osima@yahoo.it>
2014-06-07 12:54:18 +0200

    First Commit. Windows Phone 8.1 specific project using unity win phone 8.1 beta

---
+++ main/JDMaster/Assets/Scripts/Editor/EditorTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class EditorTest  : EditorWindow
+{
+    public int test = 10;
+    public float anothertest = 20.0f;
+
+    [MenuItem("Window/Create Window")]
+    public static void CreateWizard() 
+    {
+        EditorWindow.GetWindow<EditorTest>();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Test Window", EditorStyles.boldLabel);
+
+        GUILayout.BeginVertical("box");
+        GUILayout.Label("a value", "box");
+        GUILayout.Label("another value", "box");
+        GUILayout.EndVertical();
+    }
+}
